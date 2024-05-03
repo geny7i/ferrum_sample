cdc9a295af31e2c079f7610a61bd3b513ce7f83c
geny7i <38778469+geny7i@users.noreply.github.com>
2024-05-04 00:20:16 +0900

    ferrumを使ったPDF出力実装

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -5,6 +5,38 @@ class PdfController < ApplicationController
   end
 
   def ferrum
-    # TODO: generate PDF with Ferrum
+    @users = DummyUser.all
+    pdf = generate_pdf(template: 'pdf/index')
+    send_data pdf, filename: "Ferrumサンプル.pdf", type: "application/pdf", disposition: "inline"
+  end
+
+  private
+
+  def generate_pdf(layout: 'pdf', template: nil, **pdf_options)
+    # viewファイルをHTMLに変換
+    html_content = render_to_string(layout:, template:, formats: :html)
+    # HTMLをPDFに変換
+    html_to_pdf(html_content, **pdf_options)
+  end
+
+  def html_to_pdf(html_content, format: :A4, encoding: :binary, scale: 1.0, print_background: true)
+    browser = Ferrum::Browser.new(browser_options: { "no-sandbox": nil }, js_errors: true)
+    page = browser.create_page
+
+    # HTMLからdataURLを生成して開く
+    page.content = html_content
+
+    # メディアタイプをscreenに設定
+    page.command("Emulation.setEmulatedMedia", media: "screen")
+
+    # PDFを生成
+    page.pdf(
+      format:,
+      encoding:,
+      scale: scale * 0.8,
+      print_background:,
+    )
+  ensure
+    browser&.quit
   end
 end
